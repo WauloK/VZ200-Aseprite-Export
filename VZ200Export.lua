e0b2e2acabe767760e682f75cc9ff64a0aff6905
Jason Oakley <waulok@gmail.com>
2021-11-14 12:24:48 +1100

    Now also exports to TRSE byte array format in Decimal

--- VZ200Export.lua
+++ VZ200Export.lua
@@ -24,7 +24,7 @@ if (sprite.width % 4) ~= 0 then
   return
 end
 
--- Get and convert pixel data
+-- Get and convert pixel data in Hex Assembly format
 local function getTileData(img, x, y)
     local res = ""
 
@@ -52,10 +52,35 @@ local function getTileData(img, x, y)
     return "DB " .. res .. "\n"
 end
 
+-- Get and convert pixel data in TRSE Decimal Array format
+local function getTileDataTRSE(img, x, y)
+    local res = ""
+
+    for  cy = 0, sprite.height-1 do
+        local val = 0
+        -- VZ200 has 2 bits per pixel and 4 pixels per byte
+        for cx = 0, sprite.width-1, 4 do
+            value = 0
+            for xbyte = 0, 3 do
+                px = img:getPixel(cx+x+xbyte, cy+y)
+                -- Index 0 is transparent and not used for VZ200
+                px = px - 1
+                value = value << 2 | px
+            end
+            res = res .. string.format("%03d", value)
+            if cy < sprite.height then
+                res = res .. ", "
+            end
+        end
+    end
+    return res
+end
+
 local spriteLookup = {}
 local lastLookupId = 0
+local choice = ""
 
--- Export frame data - used in single or all sprite output
+-- Export frame data in Assembly Hex format - used in single or all sprite output
 local function exportFrame(useLookup, frm)
     if frm == nil then
         frm = 1
@@ -68,8 +93,16 @@ local function exportFrame(useLookup, frm)
 
     for x = 0, sprite.width-1, sprite.width do
         local column = {}
+        local data = ""
         for y = 0, sprite.height-1, sprite.height do
-            local data = getTileData(img, x, y)
+            if choice.trseDecArrayFormat then
+                data = getTileDataTRSE(img, x, y)
+                if frm == #sprite.frames or choice.onlyCurrentFrame then
+                    data = string.sub(data,1,-3)
+                end
+            else
+                data = getTileData(img, x, y)
+            end
             local id = 0
             if useLookup then
                 id = spriteLookup[data]
@@ -92,7 +125,6 @@ local function exportFrame(useLookup, frm)
         end
         table.insert(result, column)
     end
-
     return result
 end
 
@@ -115,33 +147,47 @@ dlg:file{ id="exportFile",
           title="VZ200-Assembler Export",
           open=false,
           save=true,
-          filetypes={"asm", "inc", "z80" }}
+          filetypes={"asm", "inc", "z80", "txt" }}
 
 dlg:check{ id="onlyCurrentFrame",
            text="Export only current frame",
            selected=true }
+dlg:newrow()           
 dlg:check{ id="removeDuplicates",
            text="Remove duplicate tiles",
            selected=false}
+dlg:newrow()           
+dlg:check{ id="trseDecArrayFormat",
+           text="Create TRSE Array data in Decimal",
+           selected=false}         
 
 dlg:button{ id="ok", text="OK" }
 dlg:button{ id="cancel", text="Cancel" }
 dlg:show()
-local data = dlg.data
-if data.ok then
-    local f = io.open(data.exportFile, "w")
+choice = dlg.data
+
+if choice.ok then
+    local f = io.open(choice.exportFile, "w")
     io.output(f)
 
     local mapData = {}
 
-    if data.onlyCurrentFrame then
-        table.insert(mapData, exportFrame(data.removeDuplicates, app.activeFrame))
-    else
-        for i = 1,#sprite.frames do
-            io.write(string.format(";Frame %d\n", i))
-            table.insert(mapData, exportFrame(data.removeDuplicates, i))
-        end
+    if choice.trseDecArrayFormat then
+        io.write("spriteData: array[] of byte =(")
     end
 
+    if choice.onlyCurrentFrame then
+        table.insert(mapData, exportFrame(choice.removeDuplicates, app.activeFrame))
+    else
+        for i = 1,#sprite.frames do
+            if not choice.trseDecArrayFormat then
+                io.write(string.format(";Frame %d\n", i))
+            end
+            table.insert(mapData, exportFrame(choice.removeDuplicates, i))
+        end
+    end
+    if choice.trseDecArrayFormat then
+        io.write(")")
+    end    
     io.close(f)
 end
